4809843adb75b22d9b74e8318fd5e25f35c897dd
Sedra <83130573+siudro@users.noreply.github.com>
2021-10-19 20:10:31 +0300

    Add files via upload

---
+++ OSLabs/calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int firstNum,secondNum;
+	cout<< "Enter first number\n";
+	cin>> firstNum;
+	cout<< "Enter second number\n";
+	cin>> secondNum;
+	cout<< "Sum of " <<firstNum<<" and "<<secondNum<< " is "<< firstNum+secondNum;
+	return 0;
+}
