0643817e924cb8b4e866779ef236ec2efc6e7de6
Sedra <83130573+siudro@users.noreply.github.com>
2021-12-02 19:31:19 +0300

    add arabic comments

--- OSLabs/Lab03_Process_fork/ProcessID.cpp
+++ OSLabs/Lab03_Process_fork/ProcessID.cpp
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-using namespace std;
+#include <sys/types.h> // جلب المكتبات اللازمة
+using namespace std; // خطوة لاختصار عمليات الادخال والاخراج
 
-int main(){
-	pid_t pid1, pid2, cpid;
+int main(){ // البدء في الماين
+	pid_t pid1, pid2, cpid; // انشاء بارينت بروسس اي دي و تشايلد ببروسس اي دي
 	int i, j, a, status;
 	a= 0;
-	cout<<"\n parent process pid is "<<getpid()<<"\n";
-	pid1 = fork();
+	cout<<"\n parent process pid is "<<getpid()<<"\n"; // طباعة الآيدي للبارينت بروسس
+	pid1 = fork(); // إنشاء تشايلد بروسس
 	if (pid1<0){
 		cout<<"first fork has failed\n";
 		exit(-1);
@@ -25,7 +25,7 @@ int main(){
 
 }
 	else{
-		pid2 = fork();
+		pid2 = fork(); 
 		if (pid2 < 0){
 			cout<<"second fork nooo\n";
 			exit(-1);
