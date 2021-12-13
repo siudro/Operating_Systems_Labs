ce2d058092e3f5c549f20c8fe153d8e8fb53e1e0
Sedra <83130573+siudro@users.noreply.github.com>
2021-12-13 04:49:16 +0300

    add Arabic comments

--- OSLabs/Lab04_Threads/threadIDfunction.cpp
+++ OSLabs/Lab04_Threads/threadIDfunction.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
-#include "pthread.h"
+#include "pthread.h"// استدعاء المكتبات اللازمة
 using namespace std;
 #define NLOOP 10
-int counter = 0;
-void * function1(void*);
-void * function2(void*);
+int counter = 0;//انشاء عداد ليتم اللإضافة عليه من أطراف مختلفة 
+void * function1(void*);// تعريف دوال للإضافة على العداد
+void * function2(void*);// تعريف دوال للأضافة على العداد
 int main(){
 	
-	pthread_t tidA, tidB;
+	pthread_t tidA, tidB;// تعريف ثريدين
 	int status;
-	pthread_create(&tidA, NULL, function1, NULL);
-	pthread_create(&tidB, NULL, function2, NULL);
-	pthread_join(tidA, NULL);
-	pthread_join(tidB, NULL);
+	pthread_create(&tidA, NULL, function1, NULL);// الثريد الأول يقوم بالدخول على االدالة الأولى التي تقوم بالإضافة في العداد
+	pthread_create(&tidB, NULL, function2, NULL);// الثريد الثاني يدخل في الدالة الثانية لتقوم باللإضافة على قيمة العداد
+	pthread_join(tidA, NULL);// انتظار الثريد الأول حتى أن ينتهي من التنفيذ
+	pthread_join(tidB, NULL);// انتظار الثريد الثاني حتى ينتهي من التنفيذ
 	exit(0);
 
 }
@@ -26,7 +26,7 @@ void* function1(void *vprt){
 	cout<<" THREAD = "<<pthread_self()<<"\n";
 	cout<<" COUNTER = "<<dec<<counter<<endl;
 	sleep(1);
-	val=val+1;
+	val=val+1;// الإضافة على العداد
 } 
 	return (NULL);
 }
@@ -38,7 +38,7 @@ void* function2(void *vprt){
 	cout<<" THREAD = "<<pthread_self()<<"\n";
 	cout<<" COUNTER = "<<dec<<counter<<endl;
 	sleep(1);
-	val=val*2
+	val=val*2// الإضافة على العداد
 } 
 	return (NULL);
 }
