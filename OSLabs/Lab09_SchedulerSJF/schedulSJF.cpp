7997ae5a4849657c8bf060fe0277618af4f10c3c
Sedra <83130573+siudro@users.noreply.github.com>
2021-12-13 11:26:11 +0300

    add  Arabic comments

--- OSLabs/Lab09_SchedulerSJF/schedulSJF.cpp
+++ OSLabs/Lab09_SchedulerSJF/schedulSJF.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
-#include "pthread.h"
+#include "pthread.h"// تعريف المكتبات اللازمة
 using namespace std;
 float n, tempb, tempa, tempp, tw, average, gap,arrive[3], burst[3],
-process[3],start[3],finish[3],waiting[3],response[3], turnaround[3];
-///////////////////start finish function////////////
-void* start_finish(void *vprt)
+process[3],start[3],finish[3],waiting[3],response[3], turnaround[3]; // تعريف المتغيرات المستخدمة في الترتيب وتعريف المصفوفات  بحجم ابتدائي
+
+void* start_finish(void *vprt) // تعريف وبدء دالة حساب بداية تنفيذ البروسس ونهايته
 {
 	start[0]=arrive[0];
-	finish[0]=arrive[0]+burst[0];
-	for(int i=1;i<3;i++){
+	finish[0]=arrive[0]+burst[0];// من قانون حساب الانتهاء من التنفيذ
+	for(int i=1;i<3;i++){ // لوب لحساب فجوة مابعد تنفيذ بروسس وبداية تنفيذ بروسس جديدة واستبعادها
 		gap=0;
 		if(arrive[i]>finish[i-1]){
 			gap=arrive[i]-finish[i-1];
@@ -23,12 +23,12 @@ void* start_finish(void *vprt)
 return(NULL);
 } //end start_finish function
 int main(){
-	pthread_t tidA, tidB;
+	pthread_t tidA, tidB; // تعريف ثريدين
 	int status;
 
 	int i,j;
-////////////// Get values from User///////////
-	for(i=0;i<3;i++){ 
+
+	for(i=0;i<3;i++){ // استقبال قيم وقت وصول البروسس والبرست 
 		n=i+1;
 		process[i]=n;
 		cout<<"what is p"<<n<<" arrival time\t";
@@ -36,8 +36,8 @@ int main(){
 		cout<<" what is p"<<n<<" burst time\t";
 		cin>>burst[i];
 	}//end for
-//////////Sort process based on arrival time//////////////
-	for(i=0;i<2;i++)
+
+	for(i=0;i<2;i++) // لوب متداخل لإعادة ترتيب البروسس حسب وقت الوصول 
 		for( j=i+1;j<3;j++){
 			if(arrive[j]<arrive[i]){
 				tempa=arrive[i];
@@ -51,12 +51,11 @@ int main(){
 				process[j]=tempp;
 			}//end if
 	}//end for
-////////////calculate start and finish times /////////////
-	pthread_create(&tidA, NULL,start_finish , NULL);
-	//start_finish();
-	pthread_join(tidA, NULL);
-//////////Resort//////////////
-	for(i=0;i<2;i++)
+	
+	pthread_create(&tidA, NULL,start_finish , NULL); // إنشاء ثريد تدخل في دالة حساب وقت بداية ونهاية كل بروسس حسب ترتيبهم في وقت الوصول
+	pthread_join(tidA, NULL); // الانتظار حتى وقت نهاية تنفيذ الثريد
+
+	for(i=0;i<2;i++)// إعادة فرز وترتيب البروسس حسب وقت الوصول وقيمة البرست
 		for( j=i+1;j<3;j++){
 			if(arrive[j]<=start[i] && burst[j]<burst[i]){
 				tempa=arrive[i];
@@ -70,22 +69,19 @@ int main(){
 				process[j]=tempp;
 			}//end if
 		}//end for
-////////////calculate start and finish times /////////////
-	pthread_create(&tidB, NULL, start_finish, NULL);
-	//start_finish();
-	pthread_join(tidB, NULL);
-///calculate response, waiting, turnaround times for each process///
-	tw=0;
-	for(i=0;i<3;i++){
+
+	pthread_create(&tidB, NULL, start_finish, NULL);// إنشاء ثريد يقوم بحساب وقت بداية ونهاية تنفيذ كل بروسس بعد إعادة ترتيبهم حسب وقت الوصول والبرست
+	pthread_join(tidB, NULL);// الانتظار حتى نهاية تنفيذ الثريد
+	tw=0;// تعريف إجمالي وقت الانتظار مبدئيا بصفر
+	for(i=0;i<3;i++){ //حساب وقت الاستجابة ووقت الانتظار الزمن الكلي من بداية تنفيذه لنهايته لكل بروسس 
 		response[i]=start[i]-arrive[i];
 		waiting[i]=start[i]-arrive[i];
 		turnaround[i]=burst[i]+waiting[i];
 		tw+=waiting[i];
 	}//end for
-///calculate average waiting time///
-	average=tw/3;
-/////////////////Display results////////////////////
-	for(i=0;i<3;i++){
+	average=tw/3; // حساب متوسط وقت الانتظار
+
+	for(i=0;i<3;i++){ // طباعة الحسابات لكل بروسس
 	cout<<"process Number"<<process[i]<<'\n'<<"arrive at "<<arrive[i]<<'\n'<<"waiting Time = "<<waiting[i]<<'\n'<<"response Time= "<<response[i]<<'\n'<<"Turnaround Time = "<<turnaround[i]<<'\n';
 	}
 	cout<<"Total waiting time = "<<tw;
