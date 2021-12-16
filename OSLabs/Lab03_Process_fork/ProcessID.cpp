a6c02896febd993ae116426379f4f7ab541e0b1e
Sedra <83130573+siudro@users.noreply.github.com>
2021-12-16 07:52:02 +0300

    add Arabic comments

--- OSLabs/Lab03_Process_fork/ProcessID.cpp
+++ OSLabs/Lab03_Process_fork/ProcessID.cpp
@@ -11,13 +11,13 @@ int main(){ // البدء في الماين
 	a= 0;
 	cout<<"\n parent process pid is "<<getpid()<<"\n"; // طباعة الآيدي للبارينت بروسس
 	pid1 = fork(); // إنشاء تشايلد بروسس
-	if (pid1<0){
+	if (pid1<0){ // اذا كان البي أي دي أقل من صفر، معناه هناك خطأ ولم يتم عمل الفورك بشكل ناجح
 		cout<<"first fork has failed\n";
 		exit(-1);
 }
-	else if (pid1==0){
+	else if (pid1==0){// اذا البي أي دي بصفر، نتعامل مع تشايلد رقم 1 
 		cout<<"\n child pid is "<<getpid()<<"\n";
-			for (i=0;i<10;i++){
+			for (i=0;i<10;i++){ // ضرب قيمة أي في الإي عشر مرات وطباعتها
 				a=a^i;
 				cout<<"child1: a = "<<a<<"\n";
 				sleep(4);
@@ -25,22 +25,22 @@ int main(){ // البدء في الماين
 
 }
 	else{
-		pid2 = fork(); 
-		if (pid2 < 0){
+		pid2 = fork(); // عمل فورك لإنشاء تشايلد رقم اثنين
+		if (pid2 < 0){// اذا كان البي أي دي أقل من صفر، معناه هناك خطأ ولم يتم عمل الفورك بشكل ناجح
 			cout<<"second fork nooo\n";
 			exit(-1);
 
 }
-	else if (pid2==0){
+	else if (pid2==0){//  اذا البي أي دي بصفر، نتعامل مع تشايلد رقم 2
 		cout<<"\n child2 pid is "<<getpid<<"\n";
-			for (j=0; j<10;j++){
+			for (j=0; j<10;j++){// ضرب قيمة جاي في الإي عشر مرات وطباعتها
 				a=a^i;
 				cout<<"child2: a = "<<a<<"\n";
 				sleep(4);
 }
 }
 	else{
-		cpid = wait(&status);
+		cpid = wait(&status); // الانتظار حتى انتهاء التشايلد من التنفيذ
 		cout<<"\n---close parent---\n";
 		exit(0);
 }
